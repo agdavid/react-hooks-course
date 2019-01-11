a43d4f922fdb233f2126303668d2918377510ebf
agdavid <antonio.g.david@gmail.com>
2019-01-11 17:44:04 -0500

    dynamic search query with useState

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -4,19 +4,25 @@ import axios from 'axios';
 export default function App() {
 
   const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('reacthooks');
   
-  // proper way to use async/await in useEffect
   useEffect(()=>{
     getResults();
-  }, []);
+  // pass query into useEffect as a dependency
+  // on change of query, useEffect will run
+  }, [query]);
   
     const getResults = async () => {
-      const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+      const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`)
       setResults(response.data.hits);
     }
   
   return (
     <>
+      <input 
+        type="text"
+        onChange={event=>setQuery(event.target.value)}
+      />
       <ul>
         {results.map(result => (
           <li key={result.objectID}>
