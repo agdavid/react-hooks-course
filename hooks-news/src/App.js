d5bbae1f73e18b0d97ad315b82b7e1765de7e18b
agdavid <antonio.g.david@gmail.com>
2019-01-11 17:28:13 -0500

    GET request to HN search with useEffect; infinite loop

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function App() {
+
+  const [results, setResults] = useState([]);
+  
+  // in this manner, we send off infinite loop of requests
+  // useEffect is run on every (re)render
+  // the update of results, triggers another render, another useEffect, ad infinitum
+  useEffect(()=> {
+    axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+      .then(response => {
+        console.log(response.data);
+        setResults(response.data.hits);
+      })
+  });
+  
   return (
     <div>
       App
