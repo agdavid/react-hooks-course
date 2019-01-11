00d74b078baad17d405a6b33ffeb83611688c2c8
agdavid <antonio.g.david@gmail.com>
2019-01-11 14:08:28 -0500

    toggle boolean in class based component

--- react-hooks-intro/src/AppClass.js
+++ react-hooks-intro/src/AppClass.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class App extends Component {
 
   state = {
-    count: 0
+    count: 0,
+    isOn: false,
   }
 
   incrementCount = () => {
@@ -12,12 +13,27 @@ class App extends Component {
     }))
   }
 
+  toggleLight = () => {
+    this.setState( prevState => ({
+      isOn: !prevState.isOn
+    }))
+  }
+
   render() {
     return (
-      <div>
-        <div>Hello World</div>
+      <>
+        <h2>Counter</h2>
         <button onClick={this.incrementCount}>I was clicked {this.state.count} times</button>
-      </div>
+
+        <h2>Toggle Light</h2>
+        <div
+        style={{
+          height: "50px",
+          width: "50px",
+          background: this.state.isOn ? 'yellow' : 'grey'
+        }}
+        onClick={this.toggleLight} />
+      </>
     );
   }
 }
