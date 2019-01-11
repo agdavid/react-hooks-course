0370dbe8b085d4e4cb241335b32812263bd35d6d
agdavid <antonio.g.david@gmail.com>
2019-01-11 15:33:25 -0500

    remove event listener in useEffect without supportive API

--- react-hooks-intro/src/AppFunction.js
+++ react-hooks-intro/src/AppFunction.js
@@ -1,22 +1,36 @@
 import React, { useState, useEffect } from 'react';
 
+const initialLocationState = {
+    latitude: null,
+    longitude: null,
+    speed: null
+};
+
 const App = () => {
 
     // [variable, setState] = hook(initialState)
     const [count, setCount] = useState(0);
     const [isOn, setIsOn] = useState(false);
     const [mousePosition, setMousePosition] = useState({ x: null, y: null }); // initialState can be any JavaScript type; here {object}
-    // navigator is a browser API; check it out in the console
+    // navigator is a browser API
     const [status, setStatus] = useState(navigator.onLine) // initialState with value from external API
+    const [location, setLocation] = useState(initialLocationState)
 
     // side effect (interact with outside world) in functional component
     // effect function (e.g., state change) executed after every render
     // efficient way to replace componentDidMount componentDidUpdate
+
+    // navigator API does not have support like 'removeEventListener'
+    // set variable to cleanup effect    
+    let mounted = true;
+
     useEffect(() => {
         document.title = `You have clicked ${count} times`;
         window.addEventListener('mousemove', handleMouseMove);
         window.addEventListener('online', handleOnline);
         window.addEventListener('offline', handleOffline);
+        navigator.geolocation.getCurrentPosition(handleGeolocation);
+        const watchId = navigator.geolocation.watchPosition(handleGeolocation);
     
         // replicate componentWillUnmount
         // cleanup side effect on unmount
@@ -24,6 +38,8 @@ const App = () => {
             window.removeEventListener('mousemove', handleMouseMove);
             window.removeEventListener('online', handleOnline);
             window.removeEventListener('offline', handleOffline);
+            navigator.geolocation.clearWatch(watchId);
+            mounted = false;
         }
         // second argument contains "dependent values"
         // effect is conditionally triggered if values in [] change
@@ -31,6 +47,16 @@ const App = () => {
     // }, []);
         // values mean "this variable triggers change" => effect will run on change of that value
     }, [count]);
+
+    const handleGeolocation = (event) => {
+        if (mounted) {
+            setLocation({
+                latitude: event.coords.latitude,
+                longitude: event.coords.longitude,
+                speed: event.coords.speed
+            });
+        }
+    }
 
     const handleOnline = () => {
         setStatus(true);
@@ -77,6 +103,11 @@ const App = () => {
 
             <h2>Network Status - useState, useEffect Hook</h2>
             <p> You are <strong>{status ? "online" : "offline"}</strong></p>
+
+            <h2>Geolocation - useState, useEffect Hook</h2>
+            <p>Latitude is {location.latitude}</p>
+            <p>Longitude is {location.longitude}</p>
+            <p>Speed is {location.speed ? location.speed : 'O'}</p>
         </>
         
     )
