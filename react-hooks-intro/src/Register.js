7dce3a700b315ff2d21a007ef3e5dbad946a851d
agdavid <antonio.g.david@gmail.com>
2019-01-11 16:25:02 -0500

    update form values in state and set to user on submit with useState

--- react-hooks-intro/src/Register.js
+++ react-hooks-intro/src/Register.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Register() {
+    const [form, setForm] = useState({
+        username: "",
+        email: "",
+        password: ""
+    });
+
+    const [user, setUser] = useState(null);
+
+    const handleChange = (event) => {
+        setForm({
+            ...form, // use spread operator for current values
+            [event.target.name]: event.target.value // replace single form input value
+        });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setUser(form); // user = form
+    }
+
     return (
         <div
             style={{
@@ -14,18 +34,27 @@ export default function Register() {
                     alignItems: 'center',
                     justifyItems: 'center'
                 }}
+                onSubmit={handleSubmit}
                 >
                 <input 
                     type="text"
-                    placeholder="Username" />
+                    placeholder="Username"
+                    name="username"
+                    onChange={handleChange} />
                 <input 
                     type="email"
-                    placeholder="Email Address" />
+                    placeholder="Email Address"
+                    name="email"
+                    onChange={handleChange} />
                 <input 
                     type="password"
-                    placeholder="Password" />
+                    placeholder="Password"
+                    name="password"
+                    onChange={handleChange} />
                 <button type="submit">Submit</button>
             </form>
+
+            {user && JSON.stringify(user, null, 2)}
         </div>
     );
 };
