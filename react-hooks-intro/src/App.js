72d16e2a05fc973e0b37babe3730e8a8360742f6
agdavid <antonio.g.david@gmail.com>
2019-01-11 13:50:07 -0500

    simple counter function in class based component

--- react-hooks-intro/src/App.js
+++ react-hooks-intro/src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  state = {
+    count: 0
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
   render() {
     return (
-      <div>Hello World</div>
+      <div>
+        <div>Hello World</div>
+        <button onClick={this.incrementCount}>I was clicked {this.state.count} times</button>
+      </div>
     );
   }
 }
