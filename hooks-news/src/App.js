2a95de3add72cbb79f57d232e8165dc5c819a7c2
agdavid <antonio.g.david@gmail.com>
2019-01-11 17:39:36 -0500

    proper: use async/await in useEffect

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -5,16 +5,15 @@ export default function App() {
 
   const [results, setResults] = useState([]);
   
-  // improper way to use async/await in useEffect
-  useEffect(async ()=> {
-    const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
-    setResults(response.data.hits);  
-      // .then(response => {
-      //   setResults(response.data.hits);
-      // })
-    // passing the second argument lists the "dependencies" that trigger useEffect
-    // the empty array means no dependencies, run only on component mount and not update
-    }, []);
+  // proper way to use async/await in useEffect
+  useEffect(()=>{
+    getResults();
+  }, []);
+  
+    const getResults = async () => {
+      const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+      setResults(response.data.hits);
+    }
   
   return (
     <>
