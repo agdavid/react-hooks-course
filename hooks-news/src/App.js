95f81192e8a0d9e03c5aa96d3d51e6b1cd3537aa
agdavid <antonio.g.david@gmail.com>
2019-01-11 17:34:11 -0500

    display results after GET request in useEffect

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -5,20 +5,24 @@ export default function App() {
 
   const [results, setResults] = useState([]);
   
-  // in this manner, we send off infinite loop of requests
-  // useEffect is run on every (re)render
-  // the update of results, triggers another render, another useEffect, ad infinitum
   useEffect(()=> {
     axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
       .then(response => {
-        console.log(response.data);
         setResults(response.data.hits);
       })
-  });
+  // passing the second argument lists the "dependencies" that trigger useEffect
+  // the empty array means no dependencies, run only on component mount and not update
+    }, []);
   
   return (
-    <div>
-      App
-    </div>
+    <>
+      <ul>
+        {results.map(result => (
+          <li key={result.objectID}>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
