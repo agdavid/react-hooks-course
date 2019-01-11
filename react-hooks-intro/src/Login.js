0d0dd27d37615e85e652f2cfa394356e67b9a3d8
agdavid <antonio.g.david@gmail.com>
2019-01-11 15:48:48 -0500

    add form to Login

--- react-hooks-intro/src/Login.js
+++ react-hooks-intro/src/Login.js
@@ -2,6 +2,27 @@ import React from 'react';
 
 export default function Login() {
     return (
-        <div>Login</div>
+        <div
+            style={{
+                textAlign: 'center'
+            }}
+        >
+            <h2>Login</h2>
+            <form
+                style={{
+                    display: 'grid',
+                    alignItems: 'center',
+                    justifyItems: 'center'
+                }}
+                >
+                <input 
+                    type="text"
+                    placeholder="Username"/>
+                <input 
+                    type="password"
+                    placeholder="Password"/>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     );
 }
