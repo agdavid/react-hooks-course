1fc47d691bb465f461daab3bd02097dcef731c74
agdavid <antonio.g.david@gmail.com>
2019-01-12 07:44:10 -0500

    add element styling with Tailwind CSS classNames

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -35,28 +35,47 @@ export default function App() {
   }
 
   return (
-    <>
+    <div
+      className="container max-w-md mx-auto p-4 m-2 bg-purple-lightest shadow-lg rounded">
+      <img 
+        src="https://icon.now.sh/react/c0c" 
+        alt="react logo"
+        className="float-right h-12" />
+      <h1
+        className="text-grey-darkest font-thin">Hooks News</h1>
       <form
         onSubmit={handleSearch}
-      >
+        className="mb-2">
         <input 
           type="text"
           onChange={event=>setQuery(event.target.value)}
           value={query}
           ref={searchInputRef}
-        />
-        <button type="submit">Search</button>
-        <button type="button" onClick={handleClearSearch}>Clear</button>
+          className="border p-1 rounded"/>
+        <button 
+          type="submit"
+          className="bg-orange rounded m-1 p-1">Search</button>
+        <button 
+          type="button" 
+          onClick={handleClearSearch}
+          className="bg-teal text-white p-1 rounded">Clear</button>
       </form>
-      { isLoading ? <div>Loading results...</div> : [<ul>
+      { isLoading ? 
+        <div
+          className="font-bold text-orange-dark">Loading results...</div> : 
+        [<ul
+          className="list-reset leading-normal">
         {results.map(result => (
-          <li key={result.objectID}>
-            <a href={result.url}>{result.title}</a>
+          <li 
+          key={result.objectID}>
+            <a 
+              href={result.url}
+              className="text-indigo-dark hover:text-indigo-darkest">{result.title}</a>
           </li>
         ))}
       </ul>]}
 
-      {error && <div>{error.message}</div>}
-    </>
+      {error && <div>className="text-red font-bold">{error.message}</div>}
+    </div>
   );
 }
