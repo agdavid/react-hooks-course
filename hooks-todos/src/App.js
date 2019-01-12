2d47e70d40f5b3a8ea731c9123017e289549b870
agdavid <antonio.g.david@gmail.com>
2019-01-12 08:27:10 -0500

    manage state with useReducer hook

--- hooks-todos/src/App.js
+++ hooks-todos/src/App.js
@@ -1,13 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useReducer } from 'react';
 import { UserContext } from './index';
 
-export default function App() {
+const initialState = {
+  count: 0,
+}
 
-  const value = useContext(UserContext);
+function reducer(state, action) {
+  switch(action.type) {
+    case "increment":
+      return {
+        count: state.count + 1
+      }
+    case "decrement":
+      return {
+        count: state.count - 1
+      }
+    case "reset":
+      return initialState
+    default: 
+      return initialState
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // const value = useContext(UserContext);
+
 
   return (
     <div>
-      Hello, {value}
+      Count: {state.count}
+      <button
+        className="border m-1 p-1"
+        onClick={() =>dispatch({ type: "increment" })}>Increment</button>
+      <button
+        className="border m-1 p-1"
+        onClick={() =>dispatch({ type: "decrement" })}>Decrement</button>
+      <button
+        className="border m-1 p-1"
+        onClick={() =>dispatch({ type: "reset" })}>Reset</button>
+
     </div>
   );
 };
