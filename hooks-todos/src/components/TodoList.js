ca0e09e39df45aad3dea91b6ba1566ead3275012
agdavid <antonio.g.david@gmail.com>
2019-01-13 08:22:26 -0500

    toggle 'complete' of a todo with TOGGLE_TODO action type

--- hooks-todos/src/components/TodoList.js
+++ hooks-todos/src/components/TodoList.js
@@ -3,7 +3,7 @@ import TodosContext from '../context';
 
 
 export default function TodoList() {
-    const { state } = useContext(TodosContext);
+    const { state, dispatch } = useContext(TodosContext);
     const title = state.todos.length > 0 ? `${state.todos.length} Todos` : 'Nothing To Do!';
 
     return (
@@ -18,7 +18,8 @@ export default function TodoList() {
                         key={todo.id}
                         className="flex items-center bg-orange-dark border-black border-dashed border-2 my-2 py-4">
                         <span
-                            className="flex-1 ml-12 cursor-pointer">{todo.text}</span>
+                            onDoubleClick={() => dispatch({ type:'TOGGLE_TODO', payload: todo })}
+                            className={`flex-1 ml-12 cursor-pointer ${todo.complete && "line-through text-grey-darkest"}`}>{todo.text}</span>
                         <button>
                             <img 
                                 src="https://icon.now.sh/edit/0050c5"
--- hooks-todos/src/reducer.js
+++ hooks-todos/src/reducer.js
@@ -1,5 +1,15 @@
 export default function reducer(state, action) {
     switch(action.type) {
+        case 'TOGGLE_TODO':
+            const toggledTodos = state.todos.map( t => t.id === action.payload.id ?
+            { 
+                ...action.payload, 
+                complete: !action.payload.complete 
+            } : t );
+            return {
+                ...state,
+                todos: toggledTodos
+            }
         default:
             return state;
     }
